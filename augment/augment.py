1cc477e7672e381e6fee7a1c162d759e18a70e67
roger1993 <roger2016um@gmail.com>
2018-04-05 17:29:29 +0800

    v_0

--- augment/augment.py
+++ augment/augment.py
@@ -27,10 +27,17 @@ def translate(image, x, y):
     # return the translated image
     return shifted
 
+def mirror_border(image, pad=32):
+    reflect = cv2.copyMakeBorder(image,pad,pad,pad,pad,cv2.BORDER_REFLECT_101)
+    return reflect
+
 if __name__ == '__main__':
-    for angle in range(0, 30, 10):
-    # rotate the image and display it
-        img = cv2.imread("/Users/roger/Downloads/Kaggle_Bowl_2018/data/stage1_train/0acd2c223d300ea55d0546797713851e818e5c697d073b7f4091b96ce0f3d2fe/images/0acd2c223d300ea55d0546797713851e818e5c697d073b7f4091b96ce0f3d2fe.png")
-        rotated = rotate(img, angle=angle)
-        cv2.imshow("Angle=%d" % (angle), rotated)
-        cv2.waitKey(0)
+    # for angle in range(0, 30, 10):
+    # # rotate the image and display it
+    #     img = cv2.imread("/Users/roger/Downloads/Kaggle_Bowl_2018/data/stage1_train/0acd2c223d300ea55d0546797713851e818e5c697d073b7f4091b96ce0f3d2fe/images/0acd2c223d300ea55d0546797713851e818e5c697d073b7f4091b96ce0f3d2fe.png")
+    #     rotated = rotate(img, angle=angle)
+    #     cv2.imshow("Angle=%d" % (angle), rotated)
+    #     cv2.waitKey(0)
+    img = cv2.imread("/Users/roger/Downloads/Kaggle_Bowl_2018/data/stage1_train/0acd2c223d300ea55d0546797713851e818e5c697d073b7f4091b96ce0f3d2fe/images/0acd2c223d300ea55d0546797713851e818e5c697d073b7f4091b96ce0f3d2fe.png")
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
